79a65d9b71c9bea281185152c0fd6fe90239fef6
kgauzampe <kgauza.mpe@gmail.com>
2021-12-01 22:46:44 +0200

    table created

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <vipList/>
+    </div>
+</template>
+<script>
+import vipList from './components/vipList.vue'
+
+export default {
+   name: "App",
+   components: {
+       vipList
+        
+    },
+}
+</script>
+
+<style>
+#app {
+    font-family: Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: black;
+    margin-top: 60px;
+}
+
+</style>
---
+++ src/components/vipList.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+<h1>Here is the list of VIPs</h1>
+<table border="1px">
+    <tr>
+        <td>account(key)</td>
+        <td>Last claim ID</td>
+        <td>Stake balance</td>
+        <td>tier</td>
+        <td>tier history</td>
+    </tr>
+    <tr v-for="item in list" v-bind:key="item.id">
+        <td>{{item.account(key)}}</td>
+        <td>{{item.last_claim_id}}</td>
+        <td>{{item.stake_balance}}</td>
+        <td>{{item.tier}}</td>
+        <td>{{item.tier_history}}</td>
+    </tr>
+</table>
+</div>
+</template>
+<script>
+import Vue from 'Vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios,axios)
+export default {
+
+    name:"vipList",
+    data()
+    {
+        return {list:undefined}
+    },
+    mounted()
+    {
+        Vue.axios.get('https://telos.bloks.io/account/stake.start').then((response)=> {
+            this.list=response.data.data
+            console.warn(response.data.data)
+        })
+    }
+}
+</script>
